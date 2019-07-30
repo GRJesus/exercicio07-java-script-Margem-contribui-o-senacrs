b2570ac810e588cb565d71482174d9982289144e
GRJesus <rod.giu@hotmail.com>
2019-07-29 21:52:24 -0300

    Calculo de margem de contribuicao

---
+++ js/index.js
@@ -0,0 +1,5 @@
+var a = parseInt (prompt("Digite aqui o valor do veiculo"));
+var b = parseInt (prompt("Digite aqui % de quanto quer ganhar"));
+var c = parseInt (prompt("Digite aqui % dos impostos"));
+var errormessage = "Valor errado";
+var d = "Valor do veiculo é R$";
