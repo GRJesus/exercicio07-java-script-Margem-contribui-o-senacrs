c19731f197f6dbbbbca17cd9c9d5b7165d1e39cc
GRJesus <rod.giu@hotmail.com>
2019-07-31 19:42:22 -0300

    Inclusao calculo de margem

--- js/index.js
+++ js/index.js
@@ -8,7 +8,6 @@ var b = parseInt (prompt("Digite aqui % de quanto quer ganhar"));
 var c = parseInt (prompt("Digite aqui % dos impostos"));
 var errormessage = "Valor errado";
 var divider = 1 - ((b+c)/100);//
-var tax = x1 * ((c)/100);
 
 var p1 = document.createElement ("p")//valor de venda
 var p2 = document.createElement ("p")//valor de imposto
@@ -17,16 +16,16 @@ var p4 = document.createElement ("p")//margem
 
 if (divider <= 0) {
     document.body.append (errormessage);
-} else { 
+} else {
      if (divider >0) {
     var x1 = (a / (divider));
-    p1.append ("Valor de venda =R$ " + x1);
-    var x2 = tax;
-    p2.append ("Valor dos impostos =R$ " + x2);
+    p1.append ("Valor de venda =R$ " + x1.toLocaleString());
+    var x2 = x1 * ((c)/100);
+    p2.append ("Valor dos impostos =R$ " + x2.toLocaleString());
     var x3 = a;
-    p3.append ("Valor de compra =R$ " + x3);
+    p3.append ("Valor de compra =R$ " + x3.toLocaleString());
     var x4 = x1 - (x2 + x3);
-    p4.append ("Valor ganho = R$ " + x4)
+    p4.append ("Valor ganho = R$ " + x4.toLocaleString())
 
     document.body.appendChild (p1);
     document.body.appendChild (p2);
